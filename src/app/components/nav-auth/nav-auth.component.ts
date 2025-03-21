e8cb1aa81635175ec575695f2f28e2c21c2b1feb
Abdox72 <abdomostafa952001@gmail.com>
2025-03-22 01:16:09 +0200

    #signup form with validation

--- src/app/components/nav-auth/nav-auth.component.ts
+++ src/app/components/nav-auth/nav-auth.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-auth',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav-auth.component.html',
   styleUrl: './nav-auth.component.css'
 })
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormControlOptions, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  signupForm:FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3) , Validators.maxLength(30)]),
+    email: new FormControl('' , [Validators.required, Validators.email]),
+    password: new FormControl('' , [Validators.required, Validators.pattern(/^[a-zA-z0-9_@&%]{6,}$/)]),
+    confirmPassword: new FormControl(''),
+    phoneNumber: new FormControl('' , [Validators.required, Validators.pattern(/^[01][0125][0-9]{9}$/)])
+  } ,{validators: [this.validateConfirmPassword] } as FormControlOptions );
 
+  validateConfirmPassword(formGroup:FormGroup):void{
+    const pass = formGroup.get('password');
+    const confirmPass = formGroup.get('confirmPassword');
+    if(confirmPass?.value === ''){
+      confirmPass?.setErrors({required:true});
+    }else if(pass?.value !== confirmPass?.value){
+      confirmPass?.setErrors({notMatched:true});
+    }
+  }
+  onSubmitForm(form:FormGroup):void{
+    console.log(form.invalid);
+    console.log(form);
+    console.log(form.errors);
+  }
 }
